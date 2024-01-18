db336099f03719878a17630c732a9e7a1ac82a4c
Cris <cristinamihalciuc@yahoo.com>
2024-01-18 20:59:34 +0200

    Solved carusel images reference

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import ImageCarousel from "./ImageCarousel";
+import img1 from "/src/assets/projects-images/ZZD3_1.jpeg";
+import img2 from "/src/assets/projects-images/ZZD3_2.jpeg";
+import img3 from "/src/assets/projects-images/ZZD3_3.jpeg";
+import img4 from "/src/assets/projects-images/ZZD3_4.jpeg";
+import img5 from "/src/assets/projects-images/ZZD3_5.jpeg";
 
 function App() {
-  const images = [
-    "src/assets/projects-images/ZZD3_1.jpeg",
-    "src/assets/projects-images/ZZD3_2.jpeg",
-    "src/assets/projects-images/ZZD3_3.jpeg",
-    "src/assets/projects-images/ZZD3_4.jpeg",
-    "src/assets/projects-images/ZZD3_5.jpeg",
-  ];
+  const images = [img1, img2, img3, img4, img5];
 
   return (
     <>
